b7442a7ae7b332d86ab294b6e4893aae361781c0
Genis <grodriguez19012@iesjoanramis.org>
2023-10-30 19:52:19 +0100

    nuevo tema estructuras de seleccion

---
+++ practica3/practica3/src/EstructurasDeSeleccio/teoria/app.java
@@ -0,0 +1,59 @@
+package EstructurasDeSeleccio.teoria;
+
+public class app {
+    public static void main(String[] args) {
+
+        // Estructuras de seleccion
+        // if, if else
+        // switch
+
+        // condicion simple: if
+
+        // if (condition) {
+        // instrucciones...
+        // }
+
+
+        // if-else
+
+
+        // if (condition) {
+        //          instrucciones...
+        // } else if{
+        //          instrucciones...
+        // }else{
+        //          instrucciones...
+        // }
+
+
+        //switch
+
+        // solo se pueden usar numeros string numeros enteros y variables convertibles en enteros.
+
+        // switch (variable) {
+        //     case valor:
+        //         instrucciones...
+        //         break;
+        //     case valor:
+        //         instrucciones...
+        //         break;
+        
+        //     default:
+        //         instrucciones...
+        //         break;
+        // }
+
+
+        //opcion alternativa
+
+        // switch (variable) {
+        //     case valor ->{
+        //         instrucciones...
+        //     }
+        //     default ->
+        //         instrucciones...
+        // }
+
+        
+    }
+}
