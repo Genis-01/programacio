d0e115346788e6a2848e2af839c5bea4f594f1c9
Genis <grodriguez19012@iesjoanramis.org>
2023-11-08 22:45:19 +0100

    gjhsvfd

--- tema2/practica_4_4/activitat_4_4.java
+++ tema2/practica_4_4/activitat_4_4.java
@@ -2,50 +2,46 @@ package practica_4_4;
 
 import java.util.Scanner;
 
-
 public class activitat_4_4 {
     public static void main(String[] args) {
         cls();
-        System.out.println(DeDecimalABinario(PideYScaneaint("escribe un numero")));// pide un numero lo convierte a binario y lo muestra por pantalla
+        System.out.println(DeDecimalABinario(PideYScaneaint("escribe un numero")));// pide un numero lo convierte a
+                                                                                   // binario y lo muestra por pantalla
     }
 
-    public static String DeDecimalABinario(int Cociente) {//cambia de decimal a binario
-        String NumeroBinarioInverso = "";
-        String NumeroumeroBinario = "";//declara las variables necesarias
+    public static String DeDecimalABinario(int Cociente) {// cambia de decimal a binario
+        String NumeroBinario = "";// declara las variables necesarias
 
-        if (Cociente>=0) {
-            
+        if (Cociente > 1) {
+
             while (Cociente != 0 && Cociente != 1) {
-                NumeroBinarioInverso += divisionResto(Cociente);
+                NumeroBinario = divisionResto(Cociente) + NumeroBinario;
                 Cociente = divisionCociente(Cociente);
             }
-            NumeroBinarioInverso += Cociente; //hace la conversion, pero al hacerla el numero en birario queda invertido
-            
-            for (int i = NumeroBinarioInverso.length() - 1; i >= 0; i--) {
-                NumeroumeroBinario += NumeroBinarioInverso.charAt(i);
-            }   //invierte el numero binario para que quede bien
+            NumeroBinario = Cociente + NumeroBinario; // hace la conversion, pero al hacerla el numero en
+                                                                    // birario queda invertido
 
-
-            return NumeroumeroBinario; //devuelve el numero en binario
-        }else if (Cociente==1) {
+            return NumeroBinario; // devuelve el numero en binario
+        } else if (Cociente == 1) {
             return "1";
-        }else{
+        } else {
             return "0";
-        } //si el numero es 1 o 0 se salta toda la conversion
-        
-    }
-
-    public static int divisionCociente(int dividendo) {//hace una division entre 2 y devuelve el cociente
-        return dividendo / 2; 
-    }
-
-    public static int divisionResto(int dividendo) {//hace una division entre 2 y devuelve el resto
-
-        return dividendo % 2; 
+        } // si el numero es 1 o 0 se salta toda la conversion
 
     }
 
-    public static int PideYScaneaint(String Mensaje) {//muestra por pantalla un mensaje y devuelve el input del usuario (un numero)
+    public static int divisionCociente(int dividendo) {// hace una division entre 2 y devuelve el cociente
+        return dividendo / 2;
+    }
+
+    public static int divisionResto(int dividendo) {// hace una division entre 2 y devuelve el resto
+
+        return dividendo % 2;
+
+    }
+
+    public static int PideYScaneaint(String Mensaje) {// muestra por pantalla un mensaje y devuelve el input del usuario
+                                                      // (un numero)
         int numero;
         System.out.println(Mensaje);
         Scanner sc = new Scanner(System.in);
@@ -65,14 +61,12 @@ public class activitat_4_4 {
         return numero;
     }
 
-    public static void cls(){//limpia la consola
-        try
-	{	
-		new ProcessBuilder("cmd","/c","cls").inheritIO().start().waitFor();
-	}catch(Exception E)
-		{
-			System.out.println(E);
-		}
+    public static void cls() {// limpia la consola
+        try {
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        } catch (Exception E) {
+            System.out.println(E);
+        }
     }
-        
+
 }
