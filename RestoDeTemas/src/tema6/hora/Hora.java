f6eae5d28f0e7ba9f342053dbf8abd5bc3a193aa
Genis <grodriguez19012@iesjoanramis.org>
2024-01-10 19:15:32 +0100

    horas

--- RestoDeTemas/src/tema6/hora/Hora.java
+++ RestoDeTemas/src/tema6/hora/Hora.java
@@ -1,10 +1,12 @@
 package tema6.hora;
 
 public class Hora {
-    int hora;
-    int minuto;
 
-    public boolean setHora(int hora) {
+
+    int hora;
+    int minuto; // declara las variables
+
+    public boolean setHora(int hora) { // asigna un valor a la hora, tiene requisitos
         if (hora >=0 && hora <= 24) {
             this.hora = hora;
             return true;
@@ -14,7 +16,7 @@ public class Hora {
         
     }
 
-    public boolean setMinuto(int minuto) {
+    public boolean setMinuto(int minuto) { // asigna un valor a la minuto, tiene requisitos
         if (minuto >=0 && minuto <= 59) {
             this.minuto = minuto;
             return true;
@@ -23,7 +25,7 @@ public class Hora {
         }
     }
 
-    public void inc() {
+    public void inc() { //incrementa la hora en un minuto
         if (this.minuto < 59){
             this.minuto++;
         }else{
@@ -36,7 +38,7 @@ public class Hora {
         }
     }
 
-    public String toString(){
+    public String toString(){ //le da formato a la hora, listo para imprimir
         return String.format("%02d", hora) + ":" + String.format("%02d", minuto);
     }
 
--- RestoDeTemas/src/tema6/hora/test.java
+++ RestoDeTemas/src/tema6/hora/test.java
@@ -4,13 +4,19 @@ public class test {
 
     public static void main(String[] args) {
         
+        // crea un objeto de hora
         Hora hora = new Hora();
 
+        //asigna la hora
         System.out.println(hora.setMinuto(59));
         System.out.println(hora.setHora(24));
+
+        //incrementa la hora en dos minutos
         hora.inc();
         hora.inc();
 
+
+        //imprime la hora
         System.out.println(hora.toString());
         
     }
