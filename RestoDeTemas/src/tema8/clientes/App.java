55bf58d440306d5ae0b9577bd7948bbee8656855
Genis <grodriguez19012@iesjoanramis.org>
2024-02-27 17:41:28 +0100

    dar de baja

--- RestoDeTemas/src/tema8/clientes/App.java
+++ RestoDeTemas/src/tema8/clientes/App.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.Arrays;
+import java.util.Scanner;
 
 
 
@@ -24,12 +25,56 @@ public class App {
         this.clientes = copia;
     }
 
-    public void modificarCliente(String busqueda, String dni, String nombre, String numeo){
+    public void darDeBaja(){
+        int c = buscarCliente();
+        int j = 0;
+        Cliente copia[];
+        
+        copia = Arrays.copyOf(this.clientes, this.clientes.length - 1);
         for (int i = 0; i < clientes.length; i++) {
-            if (clientes[i].dni.equals(busqueda)) {
-                clientes[i] = new Cliente(dni, nombre, numeo);
+            if (i != c) {
+                copia[j] = clientes[i];
+                j++;
             }
         }
+        clientes = copia;
+        
+    }
+
+    public int buscarCliente(){
+        int c = -1;
+        String busqueda;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("escribe el DNI del cliente");
+        busqueda = sc.nextLine();
+        
+        for (int i = 0; i < clientes.length; i++) {
+            if (clientes[i].dni.equals(busqueda)) {
+                c = i;
+            }
+        }
+
+
+        if (c == -1) {
+            System.out.println("no se ha encontrado el cliente");
+            c = buscarCliente();
+        }
+        sc.close();
+        return c;
+    }
+    public void modificarCliente(){
+                int cliente = buscarCliente();
+                String nombre;
+                String numeo;
+
+                Scanner sc = new Scanner(System.in);
+
+                System.out.println("escrebe el nombre del cliente");
+                nombre = sc.nextLine();
+                System.out.println("escrebe el nuero del cliente");
+                numeo = sc.nextLine();
+                sc.close();
+                clientes[cliente] = new Cliente(clientes[cliente].dni, nombre, numeo);
     }
 
     public Cliente[] leerFicheroArrayObjectToObject(String fichero) {
@@ -49,7 +94,7 @@ public class App {
         return arr;
     }
 
-    public static void escribirFicheroArrayObjectToObject(Cliente clientes[], String fichero){
+    public void escribirFicheroArrayObjectToObject(Cliente clientes[], String fichero){
         
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fichero))) {
             oos.writeObject(clientes);
@@ -57,6 +102,8 @@ public class App {
             System.out.println(e.getMessage());
         }
     }
+
+    
 
     @Override
     public String toString() {
--- RestoDeTemas/src/tema8/clientes/testClientes.java
+++ RestoDeTemas/src/tema8/clientes/testClientes.java
@@ -1,14 +1,63 @@
 package tema8.clientes;
 
+import java.util.Scanner;
+
 public class testClientes {
     public static void main(String[] args) {
         App a = new App();
+        int opcion;
+        boolean b = true;
+        Scanner sc = new Scanner(System.in);
 
         
-        a.nuevoCliente("41610389P", "pedro", "688336229");
-        a.nuevoCliente("41610389P", "juan", "688336229");
-        System.out.println(a.toString());
-        a.salir();
+            System.out.println("1. Añadir cliente\n" +
+                    "2. Modificar Datos\n" +
+                    "3. Dar de baja cliente\n" +
+                    "4. Lista de clientes\n" +
+                    "5. Salir");
 
+            try {
+                opcion = sc.nextInt();
+                sc.nextLine();
+                switch (opcion) {
+                    case 1:
+                        String dni;
+                        String nombre;
+                        String numero;
+
+                        System.out.println("escribe el DNI del cliente");
+                        dni = sc.nextLine();
+                        System.out.println("escribe el nombre del cliente");
+                        nombre = sc.nextLine();
+                        System.out.println("escribe el numero del cliente");
+                        numero = sc.nextLine();
+
+                        a.nuevoCliente(dni, nombre, numero);
+                        break;
+                    case 2:
+                        a.modificarCliente();
+                        break;
+                    case 3:
+                        a.darDeBaja();
+                        break;
+                    case 4:
+                        System.out.println(a.toString());
+                        break;
+                    case 5:
+                        a.salir();
+                        break;
+
+                    default:
+                        System.out.println("valor invalido");
+                        break;
+                }
+            } catch (Exception e) {
+                System.out.println("valor invalido");
+            }
+            
+            
+        
+        sc.close();
+        a.salir();
     }
 }
